9b835799862aea07224ed0287f69222dab4bbe5d
Zach <hello@zacx.dev>
2023-06-16 16:40:58 -0500

    ci script

---
+++ ci/check-version.js
@@ -0,0 +1,7 @@
+const pkg = require('../package.json');
+const tag = process.env.GITHUB_REF.replace('refs/tags/v', '');
+if (tag !== pkg.version) {
+  console.error(`Version in package.json (${pkg.version}) does not match tag (${tag})`);
+  process.exit(1);
+}
+
